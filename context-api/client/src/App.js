ad0bf77c2d3c30602248168a9c942f8263d994aa
madusankanipunajith <madushankanipunajith@gmail.com>
2021-09-06 11:06:24 +0530

    New Updates

--- context-api/client/src/App.js
+++ context-api/client/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import {Route, Switch, BrowserRouter as Router, Link } from 'react-router-dom'
+import Home from './components/Home/Home';
+import Feed from './components/Feed/Feed';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World...</h1>
+      <h1>Hello World...</h1>
+      <Router>
+          <nav>
+              <ul>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/feed">Feed</Link></li>
+              </ul>
+          </nav>
+          <Switch>
+              <Route exact path="/home">
+                  <Home/>
+              </Route>
+              <Route exact path="/feed">
+                  <Feed/>
+              </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
