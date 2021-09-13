762494afbc9f85d6c3065658697c270ca747c9a6
madusankanipunajith <madushankanipunajith@gmail.com>
2021-09-13 10:12:20 +0530

    Update App.js

--- shopping-cart-complete/frontend/src/App.js
+++ shopping-cart-complete/frontend/src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
+import { useState } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import NavBar from './components/NavBar';
+import BackDrop from './components/BackDrop';
+import SideDrawer from './components/SideDrawer';
+
 
 function App() {
+  const [sideToggle, setSideToggle] = useState(false);
   return (
       <Router>
+        <NavBar click ={()=> setSideToggle(true)}/>
+        <SideDrawer show={sideToggle} click={()=> setSideToggle(false)}/>
+        <BackDrop show={sideToggle} click={()=> setSideToggle(false)}/>
         <main>
             <Switch>
               <Route exact path = "/">
-
+                  <HomeScreen/>
               </Route>
               <Route exact path = "/product/:id">
-
+                  <ProductScreen/>
               </Route>
               <Route exact path = "/cart">
-
-              </Route>
+                  <CartScreen/>
+              </Route> 
             </Switch>
         </main>
       </Router>
