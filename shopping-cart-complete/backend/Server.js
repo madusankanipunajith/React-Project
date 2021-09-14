4e865c6cd497fb9b228ca91a77f691b9e453fc5f
madusankanipunajith <madushankanipunajith@gmail.com>
2021-09-14 08:07:38 +0530

    CORS Updates

--- shopping-cart-complete/backend/Server.js
+++ shopping-cart-complete/backend/Server.js
@@ -1,17 +1,30 @@
 require("dotenv").config();
 const connectDB = require("./config/db");
 const express = require('express');
+const cors = require('cors');
 
 // routes connection
 const productRoutes = require("./routes/productRoutes");
 
 connectDB();
 
+
 const app = express(); // Initialized the application
+
+// cors policy
+app.use(cors(
+    {
+    origin:['http://localhost:3000','http://192.168.56.1:3000'],
+  
+    methods:['GET','POST','DELETE', 'PATCH'],
+    credentials: true
+    }        
+));
+
 app.use(express.json()); // middleware to pass data as json objects
 
 // Initializing routes (middleware)
-app.use('products', productRoutes);
+app.use('/products', productRoutes);
 
  
 const PORT = process.env.PORT || 5000;
