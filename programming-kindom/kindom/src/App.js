0157a1e7d4c61f8dabc36b731b4d5e09dfe20a72
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2022-10-12 19:00:20 +0530

    Add event handling part 02

--- programming-kindom/kindom/src/App.js
+++ programming-kindom/kindom/src/App.js
@@ -16,6 +16,16 @@ function App() {
     console.log('User has clicked the button');
   }
 
+  // synthatic event vs native event (native attribute provided by the react library)
+  const handleInputChange = (event) =>{
+    console.log(event.target.value);
+  }
+
+  // passing multiple attributes when handling the events (manage custom attributes)
+  const handleInput2Change = (event, name, age, obj) => {
+    console.log(event.target.value, age, name, obj);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -24,7 +34,12 @@ function App() {
         <UserProfile company="WSO2" university="Colombo" address={{ zipCode: 12345, city: "colombo" }} school={{ name: "Ananda college" }} onUpdateClick={onUpdatEvent} onUpdateClick2={onUpdatEvent2} />
       
         <button onClick={handleOkayClick}>Okay</button>
-      
+        <button onClick={() => {console.log('User has clicked the cancel button')}}>Cancel</button>
+
+        <br></br>
+
+        <input type="text" onChange = {handleInputChange} />
+        <input type="text" onChange = {(event) => handleInput2Change(event, "Madusanka", 25, {})} />
       </header>
 
 
