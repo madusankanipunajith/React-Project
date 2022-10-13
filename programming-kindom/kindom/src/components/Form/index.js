de68902d634b47946ece2d9714443e8f9c8a9d09
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2022-10-13 06:53:39 +0530

    Add form submission part 01

---
+++ programming-kindom/kindom/src/components/Form/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Form() {
+
+    const [data, setData] = React.useState();
+
+    const handleSubmit = (event) => {
+        console.log(data);
+
+        event.preventDefault();
+    }
+
+    const handleChange = (event) => {
+        //console.log(event.target.name, event.target.value);
+        setData({ ...data, [event.target.name]: event.target.type === 'checkbox' ? event.target.checked : event.target.value })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div><input type="text" name="firstName" onChange={handleChange} /></div>
+            <div><input type="text" name="lastName" onChange={handleChange} /></div>
+            <div><input type="checkbox" name='isOk' onChange={handleChange}/></div>
+            <button type='submit'>Submit</button>
+        </form>
+
+    )
+}
+
+export default Form
